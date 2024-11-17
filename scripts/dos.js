812555c9907e1c538ff43d4b39d25ed758d59cea
saurmo0108@gmail.com <saurmo0108@gmail.com>
2024-11-17 10:37:38 -0500

    fix: deploy

--- scripts/dos.js
+++ scripts/dos.js
@@ -1,16 +1,19 @@
 import axios from "axios";
-import e from "express";
 
 const url = "http://ec2-3-138-247-63.us-east-2.compute.amazonaws.com:3000/clean-code-resume";
 // const url = "http://localhost:3000/clean-code-resume";
 
 const masiveRequest = async () => {
   const requests = [];
-  const nroRequests = 1000;
+  const nroRequests = 10;
   for (let i = 0; i < nroRequests; i++) {
     requests.push(axios.get(url));
   }
+  console.log('Peticiones en curso...');
+  
   const result = await Promise.allSettled(requests);
+  console.log('Peticiones finalizadas...');
+  
   for (let index = 0; index < result.length; index++) {
     const response = result[index];
     if (response.status === "fulfilled") {
