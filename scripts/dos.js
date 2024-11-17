7a3ebd2a82ececccf454432ad634715d3b9493ba
saurmo0108@gmail.com <saurmo0108@gmail.com>
2024-11-17 11:51:48 -0500

    refactor: endpoint

--- scripts/dos.js
+++ scripts/dos.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 // const url = "http://ec2-3-138-247-63.us-east-2.compute.amazonaws.com:3000/clean-code-resume";
-const url = "http://localhost:3000/clean-code-resume";
+const url = "http://localhost:3000/books/03-nodejs";
 
 const masiveRequest = async () => {
   const requests = [];
-  const nroRequests = 10;
+  const nroRequests = 100;
   for (let i = 0; i < nroRequests; i++) {
-    console.log(`Petición ${i} de ${nroRequests}`);
     requests.push(axios.get(url));
   }
-
   console.log("Peticiones en curso...");
   const result = await Promise.allSettled(requests);
   console.log("Peticiones finalizadas...");
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { readFile } from "fs";
+import limiter from "./middleware/rate-limit.js";
+
+const app = express();
+
+// Aplicar el middleware a todas las rutas
+// app.use(limiter);
+
+/**
+ * Endpoint de ejemplo que simula una tarea intensiva
+ * - Cargando todo el contenido en memoria con readFile
+ * - Si el archivo es muy grande, puede llegar a bloquear el servidor "out of memory"
+ */
+app.get("/books/:name", (req, res) => {
+  const date = new Date();
+  const bookName = req.params.name;
+  readFile(`docs/${bookName}.pdf`, "utf8", (err, data) => {
+    if (err) {
+      res.send("Error al leer el archivo");
+    } else {
+      const time = new Date() - date;
+      const mbSize = Math.round(data.length / 1024 / 1024);
+      res.send(`Tarea realizada en ${time} ms, archivo de ${mbSize} MB`);
+    }
+  });
+});
+
+app.get("/", (req, res) => res.send("Bienvenido a la API!"));
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000");
+});
